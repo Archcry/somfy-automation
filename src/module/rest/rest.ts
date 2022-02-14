7ddad38be27ad6452cfe104b60957ddc57c0201d
Sander Koenders <sanderkoenders@gmail.com>
2022-02-14 18:44:59 +0100

    Add rest module to the project
    Add dependencies for rest module

---
+++ src/module/rest/rest.ts
@@ -0,0 +1,69 @@
+import { Express } from 'express';
+import basicAuth from 'express-basic-auth';
+import bodyParser from 'body-parser';
+import { IEventAggregator } from '../../lib/eventaggregator/eventAggregator';
+import { Somfy as SomfyEvents, SomfySetDeploymentData } from '../../events';
+import { SomfyEventData } from '../../events';
+
+export interface RestModuleOptions {
+  app: Express;
+  eventAggregator: IEventAggregator;
+  users: { [username: string]: string };
+}
+
+export const Rest = ({ app, users, eventAggregator }: RestModuleOptions) => {
+  return {
+    start: () => {
+      app.use(
+        basicAuth({
+          users: users,
+          challenge: true,
+        })
+      );
+
+      app.use(bodyParser.json());
+
+      app.post<SomfyEventData>('/shutter/up', (req, res) => {
+        eventAggregator.publish<SomfyEventData>(SomfyEvents.Up, req.body);
+
+        res.send('ok');
+      });
+
+      app.post<SomfyEventData>('/shutter/down', (req, res) => {
+        eventAggregator.publish<SomfyEventData>(SomfyEvents.Down, req.body);
+
+        res.send('ok');
+      });
+
+      app.post<SomfyEventData>('/shutter/wink', (req, res) => {
+        eventAggregator.publish<SomfyEventData>(SomfyEvents.Wink, req.body);
+
+        res.send('ok');
+      });
+
+      app.post<SomfyEventData>('/shutter/identify', (req, res) => {
+        eventAggregator.publish<SomfyEventData>(SomfyEvents.Identify, req.body);
+
+        res.send('ok');
+      });
+
+      app.post<SomfyEventData>('/shutter/stop', (req, res) => {
+        eventAggregator.publish<SomfyEventData>(SomfyEvents.Stop, req.body);
+
+        res.send('ok');
+      });
+
+      app.post<SomfyEventData>('/shutter/my', (req, res) => {
+        eventAggregator.publish<SomfyEventData>(SomfyEvents.My, req.body);
+
+        res.send('ok');
+      });
+
+      app.post<SomfySetDeploymentData>('/shutter/setDeployment', (req, res) => {
+        eventAggregator.publish<SomfySetDeploymentData>(SomfyEvents.SetDeployment, req.body);
+
+        res.send('ok');
+      });
+    },
+  };
+};
